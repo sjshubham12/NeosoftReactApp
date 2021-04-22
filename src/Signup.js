3e8a7cd5272600468dd1a474d6b87f985f9e083b
shubham jain <jainshubham039@gmail.com>
2021-04-22 21:57:23 +0530

    fix issue

--- src/Signup.js
+++ src/Signup.js
@@ -1,101 +1,78 @@
-import {useState , useEffect} from "react"
+import {Component} from "react"
 import axios from "axios"
-import { Link } from "react-router-dom"
-import { connect } from "react-redux"
-function Signup(props){
-    
-    var [formErrors , SetformErrors] = useState({})
-    var RegisterValidate = function(elements){
-        console.log("form elements" , elements)
-        var errors = {} 
-        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-        
-        if(!elements.name.value){
-            errors.name ="Name Field is Required"
-        }
-        if(!elements.password.value){
-            errors.password ="Password Field is Required"
-        }
-        if(!elements.email.value){
-            errors.email ="Email Field is Required"
-        }
-        else if (!pattern.test(elements.email.value)) {
-            errors.email ="Please Enter Valid Email"
-        }
-        
 
-        var errorKeys = Object.keys(errors)
-        if(errorKeys.length > 0)
-        return errors
-        else
-        return false
-        
-        
+
+class Signup extends Component{
+    constructor(){
+        super()
+        this.state = {
+           
+        }
+       
     }
 
-
-   
-    let register = function(){
-
-        var form = document.getElementById('register_form');
-        var errors = RegisterValidate(form.elements)
-        if(errors){
-             SetformErrors(errors)
+    register =()=>{
+        if(!this.user.email ||  !this.user.password || !this.user.name){
+            this.setState({
+                errorMessage : "Please fill Details"
+            })
         }else{
-         SetformErrors({})
-         let registerUrl ="https://apibyashu.herokuapp.com/api/register" 
-
-         axios({
-             url : registerUrl,
-             method : "post",
-             data : this.user
-         }).then((response)=>{
-             
-             
-            console.log("response from Sigunp api",response.data)
-            
-         },(error)=>{
-             console.log("error" , error)
-         })
+            let apiUrl ="https://apibyashu.herokuapp.com/api/register" 
+            axios({
+                url : apiUrl,
+                method : "Post",
+                data : this.user
+            }).then((response)=>{
+                console.log("response from Sigunp api",response.data)
+            },(error)=>{
+                console.log("....... user Details",this.user)
+            })
         }
-
-       
-       
-        
         
     }
-    return (
-        <div style={{width:"50%" , margin:"auto"}}>
-              <form id="register_form"> 
-              <div className="form-group">
+
+    user = {}
+
+    getEmail = (event)=>{
+        this.user.email = event.target.value
+    }  
+    
+    getName = (event)=>{
+        this.user.name = event.target.value
+    } 
+    
+    
+    getPassword = (event)=>{
+        this.user.password = event.target.value
+        
+    } 
+    render() {
+        return (
+            <div style={{width:"50%" , margin:"auto"}}>
+                <div className="form-group">
                 <label>Email </label>
-                <input type="email" class="form-control" name="email"/>
-                <div className="form-error">
-                        {formErrors?.email && <div> {formErrors.email}</div> }
-                    </div>
+                <input type="email"  onChange= {this.getEmail}class="form-control" />
+                    
                 </div>
                 <div className="form-group">
                 <label>Password </label>
-                <input type="password" class="form-control" name="password" />
-                <div className="form-error">
-                        {formErrors?.password && <div> {formErrors.password}</div> }
-                    </div>
+                <input type="password" onChange= {this.getPassword}class="form-control" />
+                    
                 </div>
 
                 <div className="form-group">
                 <label>Name </label>
-                <input type="text" class="form-control" name="name" />
-                <div className="form-error">
-                        {formErrors?.name && <div> {formErrors.name}</div> }
-                    </div>
+                <input type="text" onChange= {this.getName}class="form-control" />
+                    
                 </div>
-                </form>
-                <button className="btn btn-primary" onClick={register}>Register</button>
-                
-              
-                
+
+                <div style={{color : "red"}}>
+                    {this.state.errorMessage}
+                </div>
+                <button className="btn btn-primary" onClick={this.register}>Register</button>
             </div>  
-    )
+        )
+    }
 }
 
-export default connect()(Signup)
+export default Signup
